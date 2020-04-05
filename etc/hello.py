9b5a37b4d0a3c11d52f759091a5d2cf6752a31a6
Shank <alexandernegoda95@gmail.com>
2020-04-05 17:03:30 +1000

    create wsgi app

---
+++ etc/hello.py
@@ -0,0 +1,14 @@
+def app(environ, start_response):
+    """парсим часть url, которая поступает после ?. например /?value=a=1&b=3, получаем список"""
+    data = [bytes(i) for i in environ['QUERY_STRING'].split('&')]
+    #статус код
+    status = '200 OK'
+    #заголовок ответа
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    #вызов функции start_response
+    start_response(status, response_headers)
+    # возвращаем список с данными из части урла после ?
+    return data
