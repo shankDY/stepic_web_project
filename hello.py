8fee93507ab83be9f127ce000ca126d2e999754c
Shank <alexandernegoda95@gmail.com>
2020-04-14 00:51:23 +1000

    Renamed gunicorn.conf to gunicorn.py. Modify nginx.conf, hello.py, init.sh

--- hello.py
+++ hello.py
@@ -1,13 +1,10 @@
 def app(environ, start_response):
     """парсим часть url, которая поступает после ?. например /?value=a=1&b=3, получаем список"""
-    data = [bytes('\n'.join(environ['QUERY_STRING'].split('&')), encoding='ascii')]
+    data = ['\n'.join(environ['QUERY_STRING'].split('&')).encode('utf-8')]
     #статус код
     status = '200 OK'
     #заголовок ответа
-    response_headers = [
-        ('Content-type', 'text/plain'),
-        ('Content-Length', str(len(data)))
-    ]
+    response_headers = [('Content-type', 'text/plain')]
     #вызов функции start_response
     start_response(status, response_headers)
     # возвращаем список с данными из части урла после ?
--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+gunicorn -c etc/gunicorn.conf hello:app
