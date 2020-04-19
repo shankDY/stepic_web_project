c1a0364eb37bb2ed86f95336441df2c312e5dec3
Shank <alexandernegoda95@gmail.com>
2020-04-20 00:54:55 +1000

    fixing ask.urls.py, init.sh

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -17,11 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('$', include('qa.urls')),
+    path('', include('qa.urls')),
     path('login/$', include('qa.urls')),
     path('signup/$', include('qa.urls')),
-    path('question/<int:question_id>/$', include('qa.urls')),
+    path('question/<int:question_id>/', include('qa.urls')),
     path('ask/', include('qa.urls')),
-    path('popular/$', include('qa.urls')),
-    path('new/$', include('qa.urls')),
+    path('popular/', include('qa.urls')),
+    path('new/', include('qa.urls')),
 ]
--- init.sh
+++ init.sh
@@ -4,3 +4,12 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+# Updating components for course
+sudo apt-get update
+sudo apt-get install -y python3.5
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade django==2.1
+sudo pip3 install --upgrade gunicorn
