89f22df1cedc37d4cbba772cd74d2e25e4932f70
Shank <alexandernegoda95@gmail.com>
2020-05-05 00:21:25 +1000

    fixed init.sh

--- init.sh
+++ init.sh
@@ -7,10 +7,10 @@ sudo /etc/init.d/nginx restart
 
 # Updating components for course
 sudo apt-get update
-sudo apt-get install -y python3.5
-sudo apt-get install -y python3.5-dev
+sudo apt-get install -y python3.6
+sudo apt-get install -y python3.6-dev
 sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo ln -s /usr/bin/python3.6 /usr/bin/python3
 sudo pip3 install --upgrade pip
 sudo pip3 install --upgrade django==2.1
 sudo pip3 install --upgrade gunicorn
